af37678882aaed0a74eacb84b85b68f0e59e15ae
ShubhzDev <shubhaangiunity@gmail.com>
2024-10-19 18:15:10 +0530

    working creation and updation of dataset in blockchain

--- models/index.ts
+++ models/index.ts
@@ -1,20 +1,56 @@
-import { PrismaClient } from "@prisma/client";
+import { Game, Moves, Player, PrismaClient } from "@prisma/client";
+import { connect } from "http2";
 
 const prisma = new PrismaClient();
 
-function addPlayer(discordId : string,discordName : string,){
+function addPlayer(discordId : string,discordName : string,userName:string,playerName:string,game:Game,move : Moves){
     prisma.player.create({
         data:{
-
+            discordId:discordId,
+            discordName:discordName,
+            playerName:playerName,
+            userName:userName,
         },
-        select:{
-
+        include:{
+            discordId : true,
+            discordName : true,
+            playerName : true,
+            userName :true,
         }
-    })
+    });
 }
 
-function createGame(){
+function joinGame(id:number,discordId : string){
 
+    //check if player exists already in database
+   
+
+    prisma.game.update({
+        where:{
+            gameId:id,
+        },
+        data:{
+            players:{
+                connect:{id:player.id}
+            }
+        }
+    });
+}
+
+function createGame(discordId : string,gameHistory : Game){
+    
+    //check if player exists already in database
+
+    prisma.game.create({
+        data:{
+            players:{
+                connect:{id:player.id}
+            },
+            gameHistory:{
+                create:gameHistory,
+            }
+        }
+    })
 }
 
 function addMoves(){
@@ -24,4 +60,3 @@ function addMoves(){
 function updateGameHistory(){
 
 }
-
